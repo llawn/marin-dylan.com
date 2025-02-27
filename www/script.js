8e50fef538dfe86ddd525fe21eab889feff9d2b7
llawn <llawn06@gmail.com>
2025-02-27 11:31:45 +0100

    feat(script): edit dark-mode toggle
    change icon in nav bar when dark mode is toggle
    use user preferences for dark-mode
    if we cannot set dark-mode preference in localstorage, print in inside
    the console

--- www/script.js
+++ www/script.js
@@ -1,19 +1,41 @@
 document.addEventListener("DOMContentLoaded", function() {
     const toggleButton = document.getElementById("dark-mode-toggle");
+    const darkModeIcon = toggleButton.querySelector('i');
+    const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    // Check localStorage for dark mode preference
     const isDarkMode = localStorage.getItem("dark-mode") === "enabled";
-    
-    if (isDarkMode) {
+
+    // Set initial dark mode state based on localStorage or system preference
+    if (isDarkMode || prefersDarkMode) {
         document.body.classList.add("dark-mode");
+        darkModeIcon.classList.remove("nf-oct-moon");
+        darkModeIcon.classList.add("nf-oct-sun");
     }
     
+    // Togle dark mode on button click
     toggleButton.addEventListener("click", function() {
         document.body.classList.toggle("dark-mode");
+        
+        // Update icon based on dark mode state
         if (document.body.classList.contains("dark-mode")) {
             localStorage.setItem("dark-mode", "enabled");
+            darkModeIcon.classList.remove("nf-oct-moon");
+            darkModeIcon.classList.add("nf-oct-sun");
         } else {
+            darkModeIcon.classList.remove("nf-oct-sun");
+            darkModeIcon.classList.add("nf-oct-moon");
             localStorage.setItem("dark-mode", "disabled");
         }
     });
+
+    // Handle localStorage availability
+    try {
+        localStorage.setItem("test", "test");
+        localStorage.removeItem("test");
+    } catch (e){
+        console.warn("LocalStorage not available. Dark mode prefrence will not persits.");
+    }
 });
 
 document.getElementById('sendEmailLink').onclick = function(event) {
